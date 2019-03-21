0ccc3c9ed3ac731fe28eecd14bbd3d9d31c55ec9
lillyhelmersen <lillyhelmersen@users.noreply.github.com>
2019-03-21 14:59:57 -0700

    Looped teh moving

--- random.js
+++ random.js
@@ -1,11 +1,12 @@
-var pictures = document.getElementsByClassName("pic");
+setInterval(randomDiv, 2000);
 
+function randomDiv(){
+  var pictures = document.getElementsByClassName("pic");
 
-while(1){
   for (var i = 0; i < pictures.length; i++) {
     var pic = pictures[i];
-    pic.style.left = (Math.random() * 100) + "%";
-    pic.style.top = (Math.random() * 100) + "%";
+    pic.style.left = ((Math.random() * 60) + 15 )+ "%";
+    pic.style.top = ((Math.random() * 60) + 10 )+ "%";
   }
-  wait(4);
+
 }
