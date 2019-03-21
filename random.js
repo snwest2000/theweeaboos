f49119c10ccb4f696fb261686d6c202b544d9dd0
lillyhelmersen <lillyhelmersen@users.noreply.github.com>
2019-03-21 15:35:49 -0700

    Tyring to fiks mous enter

--- random.js
+++ random.js
@@ -1,12 +1,24 @@
+var pictures2 = document.getElementsByClassName("pic");
+
+for (var i = 0; i < pictures.length; i++){
+  var pic2 = pictures2[i];
+  pic.addEventListener("click", function( event ){
+      event.target.style.left = ((Math.random() * 60) + 15 )+ "%";
+      event.target.style.top = ((Math.random() * 60) + 10 )+ "%";
+    }
+  });
+
+/*
+randomDiv()
 setInterval(randomDiv, 2000);
 
 function randomDiv(){
-  var pictures = document.getElementsByClassName("pic"); 
+  var pictures = document.getElementsByClassName("pic");
 
   for (var i = 0; i < pictures.length; i++) {
     var pic = pictures[i];
     pic.style.left = ((Math.random() * 60) + 15 )+ "%";
     pic.style.top = ((Math.random() * 60) + 10 )+ "%";
-  }
 
-}
+  }
+}*/
