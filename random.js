c0410504b1eb405d2514225d68f812b7f56a1bac
lillyhelmersen <lillyhelmersen@users.noreply.github.com>
2019-03-21 15:56:49 -0700

    Done

--- random.js
+++ random.js
@@ -17,8 +17,8 @@ function randomDiv(){
 
   for (var i = 0; i < pictures.length; i++) {
     var pic = pictures[i];
-    pic.style.left = ((Math.random() * 60) + 15 )+ "%";
-    pic.style.top = ((Math.random() * 60) + 10 )+ "%";
+    pic.style.left = ((Math.random() * 70) + 10 )+ "%";
+    pic.style.top = ((Math.random() * 70) + 5 )+ "%";
 
   }
 }
