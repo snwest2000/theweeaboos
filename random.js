d6c13688ad22921dc7c75061bccdc3a439855eff
lillyhelmersen <lillyhelmersen@users.noreply.github.com>
2019-03-21 14:26:31 -0700

    Adde images, edited 123 and css

--- random.js
+++ random.js
@@ -1,7 +1,11 @@
 var pictures = document.getElementsByClassName("pic");
 
-for (var i = 0; i < pictures.length; i++) {
-  var pic = pictures[i];
-  pic.style.left = (Math.random() * 100) + "%";
-  pic.style.top = (Math.random() * 100) + "%";
+
+while(1){
+  for (var i = 0; i < pictures.length; i++) {
+    var pic = pictures[i];
+    pic.style.left = (Math.random() * 100) + "%";
+    pic.style.top = (Math.random() * 100) + "%";
+  }
+  wait(4);
 }
